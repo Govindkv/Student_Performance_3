3aa956836322cad596904cfc0b34c719d2ec773f
Govindkv <govind26663355@gmail.com>
2025-01-04 17:30:47 +0530

    Training and best model finding is completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,7 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
             params={
                 "Decision Tree": {
